3f0c992c39fc3eb3c43416ac27a14cee0cffd792
felipinas <jfss2@cin.ufpe.br>
2022-03-20 20:14:13 -0300

    feat: add the profile page

---
+++ client/src/pages/Profile/index.tsx
@@ -0,0 +1,13 @@
+import { Card } from "../../components/Card"
+import { Container } from "./styles"
+
+export const Profile = () => {
+    return (
+        <Container>
+            <Card
+                name="João Felipe"
+                email="jfss2@cin.ufpe.br"
+            />
+        </Container>
+    )
+}
