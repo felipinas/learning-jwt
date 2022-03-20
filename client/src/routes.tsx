9ff10724ca86b61bb5a42469ead899a2904b57c4
felipinas <jfss2@cin.ufpe.br>
2022-03-20 20:14:41 -0300

    feat: create route to profile page

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes as Router, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
+import { Profile } from "./pages/Profile";
 
 export const Routes = () => {
     return (
         <Router>
             <Route path="/" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
         </Router>
     )
 }
